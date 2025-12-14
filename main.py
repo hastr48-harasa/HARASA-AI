dd418cd63f5ede8d7cee43d10dd692cfb628b26c
hastr48-harasa <humaamabdullah48@gmail.com>
2025-12-14 11:17:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ from groq import Groq
 import os
 from dotenv import load_dotenv
 
-# Memuat variabel dari file .env
 load_dotenv()
 
 app = Flask(__name__)
 
-# Mengambil API Key dari environment variable yang sudah dimuat.
 AI_KEY = os.getenv("GROQ_API_KEY")
 
-# Validasi API Key
 if not AI_KEY:
     raise ValueError("GROQ_API_KEY tidak ditemukan. Pastikan Anda sudah membuat file .env dan mengisinya.")
 
@@ -41,27 +38,23 @@ def get_ai_definition(term):
         print("="*35)
         return "Gagal mendapatkan jawaban dari AI. Cek terminal untuk detail error."
 
-# Route utama sekarang untuk halaman pembuka
 @app.route('/')
 def welcome():
     return render_template('welcome.html')
 
-# Route untuk kamus dipindahkan ke /kamus
 @app.route('/kamus', methods=['GET', 'POST'])
 def food_dictionary():
-    # Variabel default
+
     search_term = None
     definition = None
     
     if request.method == 'POST':
-        # Ambil data dari form saat tombol di-klik
         search_term = request.form.get('istilah')
         
-        # Panggil AI hanya jika ada istilah yang dimasukkan
+
         if search_term:
             definition = get_ai_definition(search_term)
 
-    # Tampilkan halaman kamus dengan data yang sudah diisi
     return render_template('kamus.html', istilah=search_term, definisi=definition)
 
 if __name__ == "__main__":
